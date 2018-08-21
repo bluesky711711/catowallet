c13920c9552d0d770d5b2946db444ee457ef96fa
skyclean <skyclean906@gmail.com>
2018-08-22 02:09:38 +0800

    update

--- resources/views/layouts/app_form.blade.php
+++ resources/views/layouts/app_form.blade.php
@@ -173,7 +173,6 @@
                                     </div>
                                 </div>
                             </div>
-
                         </form>
                     </div>
                 </div>
