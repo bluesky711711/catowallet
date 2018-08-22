702754c513fee2bf95ca310b39ae9ddfa4bf0f4f
skyclean <skyclean906@gmail.com>
2018-08-22 21:58:46 +0800

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-      $schedule->command('notifier:toadmin')->everyMinute();
+      $schedule->command('notifier:toadmin')->everyFiveMinutes();
     }
 
     /**
