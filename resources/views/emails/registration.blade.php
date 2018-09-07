24131105ba7e5fa0dd4d96002e6f8b2be013a833
skyclean <skyclean906@gmail.com>
2018-09-07 11:13:39 +0800

    update

--- resources/views/emails/registration.blade.php
+++ resources/views/emails/registration.blade.php
@@ -1,3 +1,3 @@
-One user is registered as {{$name}}!
+One user is registered as {{$name}} ({{$first_name}} {{$last_name}})!
 <br/>
 please append server to this user:  http://207.148.65.130:8000/users/{{$id}}
